7b6bd3baaec17de4c9976c105cc4cae4ccacb461
Abhijith <puabhijith7@gmail.com>
2023-03-30 14:29:40 +0530

    2nd

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AppComponent } from './app.component';
 import { WelcomeComponent } from './welcome/welcome.component';
 
 const routes: Routes = [
-  { path: 'welcome', component: WelcomeComponent }
+  { path: 'welcome', component: WelcomeComponent },
+  { path: 'main', component: AppComponent},
+  
 ];
 
 @NgModule({
